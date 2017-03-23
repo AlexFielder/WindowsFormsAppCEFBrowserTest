142b8566f5627f046b60975bb1127246931d42bd
Alex.Fielder <Alex.Fielder@gmail.com>
2017-03-23 17:21:47 +0000

    initial commit

--- WindowsFormsAppCEFBrowserTest/Form1.cs
+++ WindowsFormsAppCEFBrowserTest/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,27 @@ namespace WindowsFormsAppCEFBrowserTest
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser chromeBrowser;
+
         public Form1()
         {
             InitializeComponent();
+
+            InitializeChromium();
+
+
         }
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("https://www.google.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
     }
 }
